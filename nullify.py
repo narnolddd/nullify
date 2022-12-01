e335cd40f0715c307897a1c0157dbd644eaa5fc3
narnolddd <n.a.arnold@qmul.ac.uk>
2022-12-01 17:45:40 +0000

    avoid recomputing number of events

--- nullify.py
+++ nullify.py
@@ -4,14 +4,16 @@ import os
 
 fname, folderpath, timeCol, sep, n = sys.argv[1], sys.argv[2], int(sys.argv[3]), sys.argv[4], int(sys.argv[5])
 
-graph_df = pd.read_csv(fname, sep=sep, header=None)
-
 os.system("mkdir -p "+str(folderpath))
+
+graph_df = pd.read_csv(fname, sep=sep, header=None)
+no_events = len(graph_df)
+
+time = graph_df[[timeCol]]
 
 for i in range(n):
     # shuffle the time column
-    time = graph_df[[timeCol]]
-    time = time.sample(n=len(time))
+    time = time.sample(n=no_events)
     time.reset_index(inplace=True,drop=True)
 
     # reinsert column and sort
